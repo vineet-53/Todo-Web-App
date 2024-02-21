8fe001d2c960108054311eba57cc3740368c03c6
vineet-53 <vineetsinghnegi53@gmail.com>
2024-02-21 12:32:14 +0530

    - added user authorization

---
+++ backend/middlewares/UserAuth.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+
+exports.authentication = async(req ,res , next) => { 
+
+    try { 
+        const token = req.cookies.token  || req.headers.authorization.split(" ")[1]; 
+        const user = jwt.decode(token , process.env.JWT_SECRET)
+        // save to request to the user 
+        req.user = user;
+        next()
+    }catch(err){  
+        return res.status(401).json({ 
+            sucess : false, 
+            message : "User Unauthorized", 
+        })
+    }
+}
+
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const app = express()  ;
 const PORT = process.env.PORT || 4000;
 const v1Router = require('./routers/v1/index')
+const cookieParser  = require('cookie-parser')
 
 // api version 
+app.use(cookieParser())
 app.use("/api/v1/" , v1Router)
 app.get('/', (req ,res) => { 
     return res.json({ 
