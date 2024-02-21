61907de2f5d423a1a5fa368cd1eef5afa9860cd0
vineet-53 <vineetsinghnegi53@gmail.com>
2024-02-21 12:55:45 +0530

    - added passwordEncyptionAndCompare

---
+++ backend/utils/passwordEncryptAndCompare.js
@@ -0,0 +1,19 @@
+const bcrypt  = require('bcrypt')
+exports.encrypt = async (textTohash , saltRounds ) => { 
+    try { 
+        const hashText = await bcrypt.hash(textTohash, saltRounds);
+        return hashText;
+    }catch(err){ 
+        throw new Error("Error Encrypting Password" , err.message)
+    }
+}
+exports.compare  = async (textToCompare , hashText) => { 
+    try { 
+        const result = await bcrypt.compare(someOtherPlaintextPassword, hash);
+        if(!result) { 
+            throw new Error("Password Not Matched"); 
+        }
+    }catch(err) { 
+        throw new Error("Error Comparing Password"); 
+    }
+}
