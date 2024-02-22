2b749066184099c746172be2c19adc5d564b3186
vineet-53 <vineetsinghnegi53@gmail.com>
2024-02-22 21:45:47 +0530

    - added checked property

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -16,6 +16,10 @@ const todoSchema = mongoose.Schema({
         type : String , 
         required : true, 
     },
+    checked : { 
+        type : Boolean,  
+        default : false,
+    },  
     createdAt : { 
         type :Date , 
         default : Date.now(), 
