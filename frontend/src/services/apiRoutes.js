936e9e0447bf5d2b4ee958bed2b0e98067e84bb0
vineet-53 <vineetsinghnegi53@gmail.com>
2024-08-22 08:26:00 +0530

    changes to apiroutes

--- frontend/src/services/apiRoutes.js
+++ frontend/src/services/apiRoutes.js
@@ -1,16 +1,15 @@
-const BASE_URL = process.env.REACT_APP_SECRET_NAME;
-const API_VERSION = "/api/v1";
+const BASE_URL = `${process.env.REACT_APP_SECRET_NAME}/api/v1`;
 export const userEndpoints = {
-  LOGIN_URL: BASE_URL + API_VERSION + "/users/login",
-  SIGNUP_URL: BASE_URL + API_VERSION + "/users/signup/",
-  LOGOUT_URL: BASE_URL + API_VERSION + "/users/logout",
-  SENDOTP_URL: BASE_URL + API_VERSION + "/users/send-otp",
-  DELETE_ACCOUNT_URL: BASE_URL + API_VERSION + "/users/delete-my-account",
+  LOGIN_URL: BASE_URL + "/users/login",
+  SIGNUP_URL: BASE_URL + "/users/signup/",
+  LOGOUT_URL: BASE_URL + "/users/logout",
+  SENDOTP_URL: BASE_URL + "/users/send-otp",
+  DELETE_ACCOUNT_URL: BASE_URL + "/users/delete-my-account",
 };
 
 export const todoEndpoints = {
-  CREATE_TODO_URL: BASE_URL + API_VERSION + "/todos/create-todo",
-  DELETE_TODO_URL: BASE_URL + API_VERSION + "/todos/delete-todo",
-  UPDATE_TODO_URL: BASE_URL + API_VERSION + "/todos/update-todo",
-  GET_ALL_TODOS_URL: BASE_URL + API_VERSION + "/todos/get-all-todos",
+  CREATE_TODO_URL: BASE_URL + "/todos/create-todo",
+  DELETE_TODO_URL: BASE_URL + "/todos/delete-todo",
+  UPDATE_TODO_URL: BASE_URL + "/todos/update-todo",
+  GET_ALL_TODOS_URL: BASE_URL + "/todos/get-all-todos",
 };
